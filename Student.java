501f0e46128e85c1573ebf670e37c8ea527c79ed
sabrinaachille <92241507+sabrinaachille@users.noreply.github.com>
2024-09-06 09:15:22 -0400

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,24 @@
+package org.example;
+/**
+ *
+ * @author MoaathAlrajab
+ */
+public class Student {
+
+    private String name;
+
+
+
+    //To Do 01: add a constructor to this class
+    public Student(){
+        name = "Sabrina Achille";
+    }
+
+    // To Do 02: Add setters and getters
+    public String getName() {
+        return name;
+    }
+    public void setName(String n){
+        name = n;
+    }
+}
---
+++ StudentLst.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class StudentLst {
+
+    public static void main(String[] args) {
+        Student[] students= new Student[2];
+        Scanner inScanner = new Scanner(System.in);
+
+        for(int i=0; i< students.length;i++){
+            System.out.print("Enter student's name: ");
+            String name = inScanner.nextLine();
+            Student student = new Student(name);
+
+            students[i]= student;
+
+        }
+        System.out.println(printOutput(students));
+    }
+
+    public static String printOutput(Student[] students){
+        String str="";
+
+        //To Do 03: complete the implementation of this method
+        for(Student student : student){
+            () -> System.out.print(student.getName(),"\n");
+        }
+        return str;
+    }
+
+
+}
